4224b94246c71916a34c1230516f55555374416b
Antonella Parra <jorgedaviddiaz62@gmail.com>
2024-11-27 11:41:04 -0500

    Ejemplos

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
@@ -30,11 +30,25 @@ public class Ejemplo02 {
             nota = entrada.nextDouble();
             cadenaFinal = String.format("%s%.2f\n", cadenaFinal, nota);
             entrada.nextLine(); // limpieza de buffer
-
-            System.out.println("Ingrese (s) si desea salir del ciclo ");
-            salida = entrada.nextLine();
-
             
+            System.out.println("Ingrese (s), (si), (yes) (y) si desea salir del ciclo ");
+            salida = entrada.nextLine();
+            /*
+            switch (salida){
+               case "s":
+               case "si":
+               case "yes":
+               case "y":
+                    bandera = false;
+                   break;*/
+            //}
+            
+            if (salida.equals("s") || salida.equals("si") || 
+                salida.equals("yes") || salida.equals("y")){
+                bandera = false;
+            }
+            
+                   
             
 
         } while (bandera); // (bandera==true)
